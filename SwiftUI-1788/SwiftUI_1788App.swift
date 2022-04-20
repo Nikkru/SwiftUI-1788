fdb033c75ca2c43ae6492f4533fde8097c359653
Nikkru <nkru@narod.ru>
2022-04-20 08:14:21 +0300

    Initial Commit

---
+++ SwiftUI-1788/SwiftUI_1788App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_1788App.swift
+//  SwiftUI-1788
+//
+//  Created by Nikkru on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_1788App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
