3b367e4fad379bb9d7cfd00adbd52c0eabe1625d
Nikkru <nkru@narod.ru>
2022-05-05 08:37:35 +0300

    VKSdk

--- SwiftUI-1788/SwiftUI_1788App.swift
+++ SwiftUI-1788/SwiftUI_1788App.swift
@@ -6,13 +6,29 @@
 //
 
 import SwiftUI
+import VKSdkFramework
 
 @main
 struct SwiftUI_1788App: App {
+    
+    @UIApplicationDelegateAdaptor private var appDelegate: MyAppDelegate
+    
     var body: some Scene {
         WindowGroup {
 //            LoginView()
             ContainerView()
         }
+    }
+}
+
+class MyAppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOption: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
+        return true
     }
 }
